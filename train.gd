147e5512bf7778c0f8bb7a77311854f6ea154480
deakakosbulcsu-dot <deakakosbulcsu@gmail.com>
2026-03-11 21:17:21 +0100

    no
    Removed placeholder function _ready and adjusted spacing.

--- train.gd
+++ train.gd
@@ -6,11 +6,6 @@ var speed = 0
 
 
 
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	progress_ratio += move
 	move = speed
